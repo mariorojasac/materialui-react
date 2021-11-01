7312bc42ee5a7e6e682655aa815943ab8bcabc67
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-01 11:05:36 -0400

    adding form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+
 
 function App() {
+
+  const [display, setDisplay] = useState({
+    firstName: 'random',
+    lastName: 'name',
+    enail: 'randomname@gmail.com',
+    location: 'seattle'
+  })
+
+  const [formData, setFromData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    location: ''
+  })
+
+  const handleChange = e => {
+    setFromData({...formData})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <Paper>
+          <form>
+            <TextField
+              type="text"
+              name="firstName"
+              label="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+            <TextField
+              type="text"
+              name="lastName"
+              label="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+            />
+            <TextField
+              type="text"
+              name="email"
+              label="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <TextField
+              type="text"
+              name="location"
+              label="Location"
+              value={formData.location}
+              onChange={handleChange}
+            />
+            <Button type="submit"> Submit </Button>
+          </form>
+        </Paper>
+      </Box>
     </div>
   );
 }
